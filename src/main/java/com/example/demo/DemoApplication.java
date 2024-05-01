c299b5ce7c54ed64aba637d3fa8040135e19975e
Soundarya <soundarya122@gmail.com>
2024-05-01 17:19:18 +0530

    all key values visible in opensearch now

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonParser;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.connector.opensearch.sink.FailureHandler;
+import org.apache.flink.connector.opensearch.sink.FlushBackoffType;
 import org.apache.flink.connector.opensearch.sink.OpensearchSink;
 import org.apache.flink.connector.opensearch.sink.OpensearchSinkBuilder;
 import org.apache.flink.streaming.api.datastream.DataStream;
@@ -11,10 +15,27 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.http.HttpHost;
 import org.opensearch.action.index.IndexRequest;
 import org.opensearch.client.Requests;
+import org.opensearch.common.xcontent.XContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DemoApplication {
 
+    public static Logger log = LoggerFactory.getLogger(DemoApplication.class);
+    private static class MyActionRequestFailureHandler implements FailureHandler {
+        @Override
+        public void onFailure(Throwable throwable) {
+            log.error("Failed to process action request: {}", throwable.getMessage());
+        }
+    }
+    private static MyActionRequestFailureHandler my1 = new MyActionRequestFailureHandler();
+
+
     // Read from Kafka topic "IngestorDataStreamDemo" -> Filter data -> insert in Kafka topic "target_topic"
+
+    private static Gson gson(){
+        return new Gson();
+    }
 
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -34,28 +55,46 @@ public class DemoApplication {
                 .build();
 
         DataStream<String> kafkaStream = env.fromSource(kafkaSource, WatermarkStrategy.noWatermarks(), "Kafka Source");
-        kafkaStream.print();
+//        kafkaStream.print();
+
         OpensearchSink<String> sink = new OpensearchSinkBuilder<String>()
                 .setHosts(new HttpHost("localhost", 9200, "https"))
                 .setEmitter(
                         (element, context, indexer) -> {
-                            // Create index request
-                            IndexRequest indexRequest = Requests.indexRequest()
-                                    .index("soundarya_index") // Specify your index name
-                                    .source("data", element);
+                            String id = extractId(element);
 
+                            // Create index request
+                            IndexRequest indexRequest =  Requests.indexRequest()
+                                    .index("heya2_index") // Specify your index name
+                                    .source(element, XContentType.JSON);
                             // Add index request to bulk processor
                             indexer.add(indexRequest);
                         }
                 )
+                .setFailureHandler(my1)
                 .setConnectionUsername("admin")
                 .setConnectionPassword("myPass2403")
                 .setAllowInsecure(true)
-                .setBulkFlushMaxActions(1)
+                .setBulkFlushMaxActions(1000)
+                .setBulkFlushMaxSizeMb(100)
+
+                .setBulkFlushBackoffStrategy(FlushBackoffType.EXPONENTIAL, 5, 10000)
                 .build();
 
         kafkaStream.sinkTo(sink);
         env.execute("Kafka Consumer Example");
     }
 
+    private static String extractId(String json){
+        return JsonParser.parseString(json)
+            .getAsJsonObject()
+            .get("meta")
+            .getAsJsonObject()
+            .get("id")
+            .getAsString();
+    }
+
+
+
 }
+
